20158d2403c220e2f142d2610fd4dc97e66fba53
CristianaVargas <www.khrys@hotmail.com>
2015-10-12 23:34:26 -0300

    Update Alice.pl

--- t8/Alice.pl
+++ t8/Alice.pl
@@ -1,6 +1,6 @@
 % Alice no País das Maravilhas
 
-% menina(Nome,Dia_semana,Horario).
+% menina(Nome,Horario).
 
 before(A,B,[A|R]):- member(B,R), !.
 before(A,B,[H|T]) :- B \== H ,before(A,B,T).
@@ -17,4 +17,4 @@ solucao(Teste) :-
 	member(menina(lia,8),Teste), % Lia deve ser testada as 8
 	member(menina(rute,8),Teste), % Rute deve ser testada as 8
 	mesmoDia(menina(olga,_),menina(mel,_),Teste),	% Olga e Mel devem ser testadas no mesmo dia 
-	member(menian(tina,_),Teste).  % Tina pode ser testada a qualquer momento 
+	member(menina(tina,_),Teste).  % Tina pode ser testada a qualquer momento 
